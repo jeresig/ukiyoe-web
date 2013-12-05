9a69021fd6b20f3c7be7938f4e691b6246a48394
John Resig <jeresig@gmail.com>
2013-12-04 23:39:42 -0500

    Simple utility for resetting bio names.

---
+++ utils/reset-bio-names.js
@@ -0,0 +1,37 @@
+var mongoose = require("mongoose"),
+    request = require("request"),
+    Bio = require("../app/models/bio"),
+    Artist = require("../app/models/artist"),
+    romajiName = require("romaji-name"),
+    async = require("async");
+
+mongoose.connect('mongodb://localhost/extract');
+
+mongoose.connection.on('error', function(err) {
+    console.error('Connection Error:', err)
+});
+
+mongoose.connection.once('open', function() {
+    console.log("Finding bios...");
+    Bio.find({source: "bm"}, function(err, bios) {
+        async.eachLimit(bios, 1, function(bio, callback) {
+            var newName = romajiName.parseName(bio.name.original);
+
+            if (newName.given !== bio.name.given ||
+                    newName.surname !== bio.name.surname ||
+                    newName.generation !== bio.name.generation) {
+                console.log("Updating: %s to %s", bio.name.name, newName.name);
+                console.log("Original: " + newName.original)
+                //bio.name = newName;
+                //bio.save(callback);
+                callback();
+            } else {
+                //console.log("\t" + bio.name.name);
+                callback();
+            }
+        }, function() {
+            console.log("DONE");
+            process.exit(0);
+        });
+    });
+});
