e7ac1cad93177effcc9a2efff5a79054bec8bae3
John Resig <jeresig@gmail.com>
2014-06-08 14:17:16 -0400

    Pull the Era/Artist home page details from the DB.

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -4,39 +4,30 @@
  */
 module.exports = function(ukiyoe) {
 
-var ages = require("../../data/ages.json");
-var highlighted = require("../../data/highlighted-artists.json");
+var Era = ukiyoe.db.model("Era");
 
 var exports = {};
 
-ages.forEach(function(age) {
-    age.artists = [];
-
-    for (var name in highlighted) {
-        var artist = highlighted[name];
-
-        if (artist.age === age.name) {
-            var parts = artist.image.split("/");
-            artist.thumb =
-                "http://data.ukiyo-e.org/" + parts[0], "thumbs", parts[1] + ".jpg";
-            //artist.artist = site.artistMap[name];
-            age.artists.push(artist);
-        }
-    }
-
-/*
-    age.artists = age.artists.sort(function(a, b) {
-        return b.artist.matches - a.artist.matches;
-    });
-*/
-});
-
 exports.index = function (req, res) {
-    res.render("home/index", {
-        title: req.i18n.__("Japanese Woodblock Print Search"),
-        desc: req.i18n.__("Japanese Woodblock print search engine. Searches thousands of Ukiyo-e, Meiji, Shin Hanga, and Sosaku Hanga prints."),
-        ages: ages,
-        total: "200,000"
+    Era.find({}, function(err, eras) {
+        async.eachLimit(eras, 4, function(era, callback) {
+            Artist.find({eras: era._id, printCount: {$gte: 100}})
+                .sort("printCount")
+                .populate("repImage")
+                .exec(function(err, artists) {
+                    era.artists = artists;
+                    callback();
+                });
+        }, function() {
+            Image.count(function(err, total) {
+                res.render("home/index", {
+                    title: req.i18n.__("Japanese Woodblock Print Search"),
+                    desc: req.i18n.__("Japanese Woodblock print search engine. Searches thousands of Ukiyo-e, Meiji, Shin Hanga, and Sosaku Hanga prints."),
+                    eras: eras,
+                    total: total
+                });
+            });
+        });
     });
 };
 
